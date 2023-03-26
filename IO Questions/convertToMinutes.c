fa2c590eedb63ea39dd376c8ca7ee04f761d009d
Vishal Sisodiya <vishal3330a@gmail.com>
2023-03-26 21:34:20 +0530

    First Commit

--- IO Questions/convertToMinutes.c
+++ IO Questions/convertToMinutes.c
@@ -1,4 +1,11 @@
-
+/*
+  Write a C program that takes hours and minutes as input, and calculates the total number 
+  of minutes.
+  Expected Output :
+  Input hours: 5
+  Input minutes: 37
+  Total: 337 minutes.
+*/
 #include <stdio.h>
 int main()
 {
