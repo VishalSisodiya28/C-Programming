173590ff6f7f885b9128b6fda05f4dd2d227fe8b
Vishal Sisodiya <vishal3330a@gmail.com>
2023-03-28 18:53:48 +0530

    First Commit

--- Conditional Questions/swappingWithoutUsingThirdVar.c
+++ Conditional Questions/swappingWithoutUsingThirdVar.c
@@ -1,23 +1,23 @@
-//18)Accept two numbers from user and swap their values without using third variable
+// Accept two numbers from user and swap their values without using third variable
 
-#include<stdio.h>
-int main(){
-  
-    int a,b;
-  
+#include <stdio.h>
+int main()
+{
+
+    int a, b;
+
     printf("Enter value of A : ");
-    scanf("%i",&a);
-    
+    scanf("%i", &a);
+
     printf("Enter value of B : ");
-    scanf("%i",&b);
-    
-    a=a+b;
-    b=a-b;
-    a=a-b;
-    
-    printf("\nSwapped value of A is : %i",a);
-    printf("\nSwapped value of B is : %i",b);
-    
-    
+    scanf("%i", &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("\nSwapped value of A is : %i", a);
+    printf("\nSwapped value of B is : %i", b);
+
     return 0;
 }
