1a6932e714e3c06346d72a26010c42436ab50fbb
Vishal Sisodiya <vishal3330a@gmail.com>
2023-03-28 18:58:13 +0530

    First Commit

---
+++ Loops Questions/HelloWorldNTimes.c
@@ -0,0 +1,18 @@
+// 19 - Accept an integer and Print hello world n times
+
+#include <stdio.h>
+int main()
+{
+    int n, i;
+   
+    printf("Enter number : \n");
+    scanf("%i", &n);
+    
+    for (i = 1; i <= n; i++)
+    {
+
+        printf("Hello World\n");
+    }
+
+    return 0;
+}
