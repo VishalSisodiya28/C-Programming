b47ac5419687914bade872e7098505dab132ceca
Vishal Sisodiya <vishal3330a@gmail.com>
2023-04-12 19:05:37 +0530

    First Commit

---
+++ Array Questions/arrayRightRotation.c
@@ -0,0 +1,39 @@
+// Array right rotation by K elements
+
+#include <stdio.h>
+
+int main()
+{
+
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int count = sizeof(arr) / sizeof(int);
+    int arr2[count];
+    int k;
+
+    printf("How many rotations ? ");
+    scanf("%d", &k);
+
+    int progress = 0;
+
+    for (int i = count - k; i < count; i++)
+    {
+
+        arr2[progress] = arr[i];
+        progress++;
+    }
+
+    for (int i = 0; i < count - k; i++)
+    {
+
+        arr2[progress] = arr[i];
+        progress++;
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+
+        printf("%d ", arr2[i]);
+    }
+
+    return 0;
+}
