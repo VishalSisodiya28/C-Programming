7acd0600efc50cbfc1bec6e085c3793f31f51adc
Vishal Sisodiya <vishal3330a@gmail.com>
2023-04-21 12:50:56 +0530

    First Commit

---
+++ Strings Questions/countingTheWord.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[] = "the Vishal Sisodiya is thethe thethe thethe ";
+
+    int n = strlen(str);
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (str[i] == 't' && i < n - 2 && str[i + 1] == 'h' && str[i + 2] == 'e')
+        {
+
+            if (i == 0 || str[i - 1] == ' ')
+            {
+
+                if (i + 3 == '\0' || str[i + 3] == ' ')
+                {
+                    count++;
+                }
+            }
+        }
+    }
+
+    printf("'the' appeared %d times\n", count);
+
+    return 0;
+}
