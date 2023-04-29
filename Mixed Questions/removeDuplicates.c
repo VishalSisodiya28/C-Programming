0529042ef38613a1f893d4467a8bd319a8d7cbe5
Vishal Sisodiya <vishal3330a@gmail.com>
2023-04-29 15:48:02 +0530

    First Commit

---
+++ Mixed Questions/removeDuplicates.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of elements : ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Insert elements : ");
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+
+                for (int k = j + 1; k < n; k++)
+                {
+                    arr[k - 1] = arr[k];
+                }
+
+                n--;
+                i--;
+            }
+        }
+    }
+    printf("Array after removing duplicates elements : ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
