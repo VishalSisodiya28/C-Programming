edefe8a1ee46ef139f77ee9bf8230607cc5856a5
Vishal Sisodiya <vishal3330a@gmail.com>
2023-05-02 18:55:30 +0530

    First Commit

---
+++ Mixed Questions/pattern3.c
@@ -0,0 +1,31 @@
+/*
+ Inverted Right Triangle
+    * * * * *
+    * * * *
+    * * *
+    * *
+    *
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Number of rows : ");
+    scanf("%d", &n);
+
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
