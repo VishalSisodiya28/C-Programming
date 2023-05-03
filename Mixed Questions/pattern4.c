cded354f01941d27e5833f7b3781f659fd015960
Vishal Sisodiya <vishal3330a@gmail.com>
2023-05-03 17:23:45 +0530

    First Commit

---
+++ Mixed Questions/pattern4.c
@@ -0,0 +1,42 @@
+/*
+Mirrored Right Triangle
+
+            *
+          * *
+        * * *
+      * * * *
+    * * * * *
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Number of rows : ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= n; j++)
+        {
+            if (j < n + 1 - i)
+            {
+                printf(" ");
+            }
+
+            else
+            {
+
+                printf("*");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
