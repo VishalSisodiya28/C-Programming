c477c22f51f315546af9e47f572c9d5f9a196540
Vishal Sisodiya <vishal3330a@gmail.com>
2023-05-05 18:28:32 +0530

    First Commit

--- Mixed Questions/pattern4.c
+++ Mixed Questions/pattern4.c
@@ -13,32 +13,31 @@ Mirrored Right Triangle
 
 int main()
 {
-    int n;
+  int n;
 
-    printf("Number of rows : ");
-    scanf("%d", &n);
+  printf("Number of rows : ");
 
-    for (int i = 1; i <= n; i++)
+  scanf("%d", &n);
+
+  for (int i = 1; i <= n; i++)
+  {
+
+    for (int j = 1; j <= n; j++)
     {
-
-        for (int j = 1; j <= n; j++)
-        {
-            if (j < n + 1 - i)
-            {
-                printf(" ");
-            }
-
-            else
-            {
-
-                printf("*");
-            }
-        }
-
-        printf("\n");
-     
+      if (j < n + 1 - i)
+      {
+        printf(" ");
       }
 
-    return 0;
+      else
+      {
 
+        printf("*");
+      }
+    }
+
+    printf("\n");
+  }
+
+  return 0;
 }
