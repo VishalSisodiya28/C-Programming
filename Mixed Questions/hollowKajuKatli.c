89d4405a8cb8b0c3efa6129a825e1a23e4808416
Vishal Sisodiya <vishal3330a@gmail.com>
2023-05-09 21:19:11 +0530

    First Commit

---
+++ Mixed Questions/hollowKajuKatli.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of rows : ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < (n - i) - 1; j++)
+        {
+            printf("*");
+        }
+
+        for (int k = 0; k < 2 * i; k++)
+        {
+
+            printf(" ");
+        }
+        for (int l = 0; l < (n - i) - 1; l++)
+        {
+            printf("*");
+        }
+
+        if (i < n - 1)
+            printf("\n");
+    }
+
+    printf("\r");
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+
+        for (int j = 0; j < (n - i) - 1; j++)
+        {
+            printf("*");
+        }
+
+        for (int k = 0; k < 2 * i; k++)
+        {
+
+            printf(" ");
+        }
+        for (int l = 0; l < (n - i) - 1; l++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
