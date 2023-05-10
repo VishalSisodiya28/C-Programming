6e4a4998673816754d508901cc773e2101c6bda3
Vishal Sisodiya <vishal3330a@gmail.com>
2023-05-10 18:10:03 +0530

    First Commit

--- Mixed Questions/hollowKajuKatli.c
+++ Mixed Questions/hollowKajuKatli.c
@@ -54,3 +54,60 @@ int main()
 
     return 0;
 }
+
+/*
+
+#include <stdio.h>
+
+int main()
+{
+  int n=8;
+
+  for(int i=0;i<n;i++){
+
+      for(int j=0;j<n-i;j++){
+
+         printf(" ");
+
+      }
+     printf("*");
+
+
+
+      for(int l=0;l<2*i-1;l++)
+        printf(" ");
+
+
+      if(i!=0)
+      printf("*");
+
+      printf("\n");
+
+  }
+
+  for(int i=n-2;i>=0;i--){
+
+      for(int j=0;j<n-i;j++){
+
+         printf(" ");
+
+      }
+     printf("*");
+
+
+
+      for(int l=0;l<2*i-1;l++)
+        printf(" ");
+
+
+      if(i!=0)
+      printf("*");
+
+      printf("\n");
+
+  }
+
+    return 0;
+}
+
+*/
