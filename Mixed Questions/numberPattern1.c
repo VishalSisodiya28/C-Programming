9629f552073c322f82a33adddca3465b90b666ec
Vishal Sisodiya <vishal3330a@gmail.com>
2023-05-13 19:04:22 +0530

    First Commit

---
+++ Mixed Questions/numberPattern1.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2 * (n - i) - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++)
+        {
+            printf("%d ", i + 1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
