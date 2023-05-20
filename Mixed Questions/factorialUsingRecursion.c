7393b34a3453a7a30f7962b600c4dc78c9961f71
Vishal Sisodiya <vishal3330a@gmail.com>
2023-05-20 15:38:01 +0530

    First Commit

---
+++ Mixed Questions/factorialUsingRecursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fact(int n)
+{
+    if (n > 0)
+        return n * fact(n - 1);
+
+    else
+        return 1;
+}
+int main()
+{
+    int factorial = fact(4);
+
+    printf("%d", factorial);
+
+    return 0;
+}
