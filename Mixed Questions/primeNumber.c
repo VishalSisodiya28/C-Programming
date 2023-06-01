6e3aeafd206236951ae4d0d4fc118312c1f5b3a2
Vishal Sisodiya <vishal3330a@gmail.com>
2023-06-01 21:26:35 +0530

    First Commit

---
+++ Mixed Questions/primeNumber.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void printPrimeNumbers(int range)
+{
+
+    if (range <= 0)
+    {
+
+        printf("Invalid Range!");
+
+        return;
+    }
+
+    for (int i = 1; i <= range; i++)
+    {
+
+        int flag = 0;
+
+        for (int j = 2; j < i; j++)
+        {
+
+            if (i % j == 0 && i != 2)
+            {
+
+                flag = 1;
+            }
+        }
+
+        if (flag == 0 && i > 1)
+        {
+
+            if (i == 2)
+                printf("%d", i);
+
+            else
+                printf(", %d", i);
+        }
+    }
+}
+
+int main()
+{
+
+    int range;
+
+    printf("Enter a range : ");
+    scanf("%d", &range);
+
+    printPrimeNumbers(range);
+
+    return 0;
+}
